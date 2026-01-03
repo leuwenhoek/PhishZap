91ef13f6623173444ebd68a97afdb1cdf7465e09
leuwenhoek <ninjabeastyy24@gmail.com>
2026-01-03 18:02:20 +0530

    COMPLETED: URL checker frontend

--- Web/app.py
+++ Web/app.py
@@ -1,15 +1,25 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import pickle
 import os
 import re
 
-absolute_path_file = os.path.abspath("Model")
+app = Flask(__name__)
+
 vector = pickle.load(open(os.path.join('Model',"vectorize.pkl"),'rb'))
 model = pickle.load(open(os.path.join('Model',"model.pkl"),'rb'))
 
-url = "github.com"
-
-cleaned_url = re.sub(r'^https?://(www\.)?','',url)
-predict = model.predict(vector.transform([cleaned_url]))[0]
-
-app
+@app.route('/URL_check', methods=['GET', 'POST'])
+def URL_check():
+    if request.method == 'POST':
+        url = request.form.get('url') 
+        cleaned_url = re.sub(r'^https?://(www\.)?','', url)        
+        prediction = model.predict(vector.transform([cleaned_url]))[0]
+        print(f"URL: {url} | Prediction: {prediction}")
+        if str(prediction).lower() == "bad" or prediction == 1:
+            return "Bad URL"
+        else:
+            return "Healthy URL"
+    
+    return render_template('url_check.html')
+if __name__ == "__main__":
+    app.run(debug=True)
